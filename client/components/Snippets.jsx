902fd89719d384f5a9b3c5fc80b9a902ecb8e0ef
Sam Liebow <sam.liebow@gmail.com>
2018-10-13 14:59:20 -0700

    Add basic hide/show logic to Snippets

--- client/components/Snippets.jsx
+++ client/components/Snippets.jsx
@@ -4,6 +4,7 @@ import snippetData from '../../snippets';
 
 class Snippets extends Component {
   state = {
+    show: false,
     snippetIndex: 0,
   };
 
@@ -22,34 +23,50 @@ class Snippets extends Component {
     snippetIndex: Math.min(this.state.snippetIndex - 1, 0)
   })
 
+  toggleShow = () => {
+    this.setState({ show: !this.state.show });
+  };
+
   render() {
-    const promptSnippets = snippetData[this.props.prompt];
-    const snippet = promptSnippets ? promptSnippets[this.state.snippetIndex] : '';
+    const {
+      show,
+      snippetIndex,
+    } = this.state;
+    const promptSnippets = snippetData[this.props.promptSelected];
+    const snippet = promptSnippets ? promptSnippets[snippetIndex] : '';
     return (
       <div>
-        <textarea 
-          id="snippetField"
-          value={snippet}
-          readOnly
-        />
-        <button
-          onClick={this.decrement}
-        >
-          Go back
-        </button>
-        <button 
-          className="copy"
-          data-clipboard-target="#snippetField"
-        >
-          Copy
-        </button>
-        <button 
-          className="copy"
-          onClick={this.increment}
-          data-clipboard-target="#snippetField"
-        >
-          Copy and go forward
-        </button>
+        <h4 onClick={this.toggleShow}> Prompt snippets </h4>
+        {show ?
+          <div>
+            <textarea
+              id="snippetField"
+              value={snippet}
+              readOnly
+            />
+            <button
+              onClick={this.decrement}
+            >
+              Go back
+            </button>
+            <button
+              className="copy"
+              data-clipboard-target="#snippetField"
+            >
+              Copy
+            </button>
+            <button
+              className="copy"
+              onClick={this.increment}
+              data-clipboard-target="#snippetField"
+            >
+              Copy and go forward
+            </button>
+          </div> :
+          <span>
+            &nbsp;...
+          </span>
+        }
       </div>
     )
   }
